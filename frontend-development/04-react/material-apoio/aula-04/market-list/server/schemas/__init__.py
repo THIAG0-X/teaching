e7e7ec0982486e90b3157897659cf8a1aec4a458
THIAG0-X <thiago.games2005@outlook.com>
2026-06-09 17:52:05 -0300

    todolist versão infdev

--- frontend-development/04-react/material-apoio/aula-04/market-list/server/schemas/__init__.py
+++
@@ -1,4 +0,0 @@
-from schemas.produto import ProdutoSchema, ProdutoBuscaSchema, ProdutoViewSchema, \
-                            ListagemProdutosSchema, ProdutoDelSchema, apresenta_produtos, \
-                            apresenta_produto, apresenta_produtos
-from schemas.error import ErrorSchema
